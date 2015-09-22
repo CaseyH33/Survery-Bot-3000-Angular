aeee718a5d5f42cd40dc732c6f0b281f27e91482
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-09-22 11:20:48 -0700

    Create SurveyFactory.

---
+++ services/SurveyFactory.js
@@ -0,0 +1,16 @@
+surveyBot.factory('SurveyFactory', function SurveyFactory() {
+  var factory = {};
+  factory.responses = [];
+
+  factory.surveySubmit = function() {
+    var response = { ninjaTurtle: factory.ninjaTurtle, sandurrito: factory.sandurrito, pokemon: factory.pokemon, vgSeries: factory.vgSeries, superpower: factory.superpower, twilight: factory.twilight, mitchBSB: factory.mitchBSB };
+    factory.responses.push(response);
+    factory.ninjaTurtle = null;
+    factory.sandurrito = null;
+    factory.pokemon = null;
+    factory.vgSeries = null;
+    factory.superpower = null;
+    factory.twilight = null;
+    factory.mitchBSB = null;
+  };
+});
